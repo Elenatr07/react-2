4ecbcc6def550e91ff9278570ddbb9f8c3ce833a
Elenatr07 <e.turkina2010@yandex.ru>
2022-11-09 15:27:37 +0300

    Updates

---
+++ precache-manifest.322f14281c17b3652db60214b3f80481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29277992329cdb3bc8b5",
+    "url": "/static/css/main.18055843.chunk.css"
+  },
+  {
+    "revision": "29277992329cdb3bc8b5",
+    "url": "/static/js/main.29277992.chunk.js"
+  },
+  {
+    "revision": "1d5d7fc1fab4f318209c",
+    "url": "/static/js/1.1d5d7fc1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8f695ab07ae4663d5e33e82862136e9",
+    "url": "/index.html"
+  }
+];
